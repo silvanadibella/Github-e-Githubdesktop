3955bb1c0bffbc0c13498e8061f452824f913eab
silvanadibella <silvanadibella0@gmail.com>
2021-09-17 09:51:22 +0200

    importato AC package

---
+++ AC2D/Assets/AdventureCreator/Scripts/Save system/Editor/RememberAnimatorEditor.cs
@@ -0,0 +1,24 @@
+﻿#if UNITY_EDITOR
+
+using UnityEditor;
+using UnityEngine;
+
+namespace AC
+{
+
+	[CustomEditor (typeof (RememberAnimator), true)]
+	public class RememberAnimatorEditor : ConstantIDEditor
+	{
+		
+		public override void OnInspectorGUI ()
+		{
+			RememberAnimator _target = (RememberAnimator) target;
+			_target.ShowGUI ();
+			SharedGUI ();
+		}
+		
+	}
+	
+}
+
+#endif
